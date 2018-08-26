15af182c6ad734ff60c07c2c5b49ad5dd7c2a0fe
sushilmate <sushilmate007@gmail.com>
2018-08-26 18:12:05 +0530

    Adding HotelsController with two apis to get hotels by city & room.

---
+++ WebApiRateLimiter/WebApiRateLimiter/Controllers/HotelsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApiRateLimiter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelsController : ControllerBase
+    {
+        // GET api/hotels/city
+        [HttpGet("city")]
+        public ActionResult<IEnumerable<string>> GetHotelsByCity()
+        {
+            return new string[] { "hotel1", "hotel2" };
+        }
+
+        // GET api/hotels/room
+        [HttpGet("room")]
+        public ActionResult<IEnumerable<string>> GetHotelsByRoomType()
+        {
+            return new string[] { "hotel3", "hotel4" };
+        }
+    }
+}
