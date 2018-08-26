965a9e7482e00e4ff95db05ca9e152a2731966d5
sushilmate <sushilmate007@gmail.com>
2018-08-26 17:36:05 +0530

    Cleaning up the code, removing unnecessary usings etc.

--- WebApiRateLimiter/WebApiRateLimiter/Controllers/ValuesController.cs
+++ WebApiRateLimiter/WebApiRateLimiter/Controllers/ValuesController.cs
@@ -1,14 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace WebApiRateLimiter.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class HotelsController : ControllerBase
     {
         // GET api/values
         [HttpGet]
