2301064971a26d352ae8fee5373a3f1a0e9fc561
sushilmate <sushilmate007@gmail.com>
2018-08-28 07:06:55 +0530

    Adding test project dependancies & method to end to end test of web api.

---
+++ WebApiRateLimiter/WebApiRateLimiter.Tests/HotelsControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiRateLimiter.Tests
+{
+    [TestClass]
+    public class HotelsControllerTests : TestServerFixture
+    {
+        [TestMethod]
+        public async Task Test_HotelsController_Is_Up()
+        {
+            var response = await Client.GetAsync("api/city/bangkok");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseStrong = await response.Content.ReadAsStringAsync();
+
+            Assert.IsFalse(string.IsNullOrWhiteSpace(responseStrong.ToString()));
+        }
+    }
+}
---
+++ WebApiRateLimiter/WebApiRateLimiter.Tests/TestServerFixture.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.PlatformAbstractions;
+using System;
+using System.IO;
+using System.Net.Http;
+
+namespace WebApiRateLimiter.Tests
+{
+    public class TestServerFixture : IDisposable
+    {
+        private readonly TestServer _testServer;
+        public HttpClient Client { get; }
+
+        public TestServerFixture()
+        {
+            var builder = new WebHostBuilder()
+                   .UseContentRoot(GetContentRootPath())
+                   .UseEnvironment("Development")
+                   .UseStartup<Startup>();  // Uses Start up class from your API Host project to configure the test server
+
+            _testServer = new TestServer(builder);
+            Client = _testServer.CreateClient();
+        }
+
+        private string GetContentRootPath()
+        {
+            var integrationTestsPath = PlatformServices.Default.Application.ApplicationBasePath;
+            return Path.GetFullPath(Path.Combine(integrationTestsPath, "../../../../WebApiRateLimiter"));
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            _testServer.Dispose();
+        }
+    }
+}
